5598bfaa92de5be46b2260e974f9daf90f7f28b0
Dima Gurkalenko <dmitrij.gurkalenko@gmail.com>
2016-02-25 16:10:35 +0200

    date regexp fixed once more

--- regexp.js
+++ regexp.js
@@ -1,12 +1,12 @@
 //reqular expressions for email, date, tags, phone
 var email = /\b.+@[a-zA-Z]+\.[a-zA-Z]{2,3}\b/i;
-var date = /\b\d{1,2}\b\s[^\d][a-zA-Z]{4,8}\s\b\d{4}\b/i;
+var date = /^(?![a-zA-Z])\d{1,2}\s[a-zA-Z]{4,8}\s\d{4}$/;
 var tags = /<.+>.+<\/.+>/i;
 var phone = /^\(?(\+380)\d{2}\)?\d{7}$/;
 
 //arrays of emails, date, tags and phones
 var emailArr = ['abc@mail.ru', '123@gmail.com', 'GGG@i.ua', 'blablabla', 'aaa@1111.com'];
-var dateArr = ['23 January 2016', '4 April 1988', '12 december 1356', 'abcd12 october 1980abcd', '12 08 2014'];
+var dateArr = ['23 January 2016', '4 April 1988', '12 december 1356', 'abcd 12 october 1980 abcd', 'abcd12 october 1980abcd'];
 var tagsArr = ['<p>sometext</p>', '<h1>6752</h1>', '<a href="www.ex.ua">ex</a>', 'sometext', '<123>numbers</123'];
 var phoneArr = ['+380 93 1949 127', '(+38066)3736436', '(+380-67)-555-55-55', '(+902 11) 999 99 99', '(+bla bla) bla'];
 
